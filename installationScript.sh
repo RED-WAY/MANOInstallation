ec9ef18c53ee37337b74b241ba44707102e20b32
VitorMendesco <vitormendesco.js@gmail.com>
2022-10-28 21:35:12 -0300

    script install 1.0

--- installationScript.sh
+++ installationScript.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-#https://github.com/RED-WAY/MANOInstallation/raw/main/manOS.jar
 MANOS_URL=https://raw.githubusercontent.com/RED-WAY/MANOInstallation/main/manOS.jar
 JAVA_VERSION=11
-	
+
+clear
 echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Inicializando o instalador do man.OS"
 sleep 2
 
 echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Olá, gostaria de iniciar a instalação do man.OS? (S/N)"
 read confirm
-if [ \"$confirm\" == \"S\" ]
+if [[ \"$confirm\" == \"S\" ]]
 	then
 		clear
 		echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Verificando versão do JAVA..."
 		sleep 2
 		java -version
-		if [ $? -eq 0 ]
+		if [[ $? -eq 0 ]]
 			then
 				echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Você já possui o JAVA instalado!"
+				sleep 2
 			else
 				echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Não foi identificada nenhuma versão instalada :("
 				sleep 1
@@ -25,7 +26,7 @@ if [ \"$confirm\" == \"S\" ]
 				sleep 1
 				echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Começar instalação do JAVA (S/N)?"
 				read confirm
-			if [ \"$confirm\" == \"S\" ]
+			if [[ \"$confirm\" == \"S\" ]]
 				then
 					echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7)  Baixando repositório..."
 					sleep 2
@@ -36,7 +37,7 @@ if [ \"$confirm\" == \"S\" ]
 					sudo apt -qq update && sudo apt -qq upgrade -y
 					clear
 
-					if [ $JAVA_VERSION -eq 11 ]
+					if [[ $JAVA_VERSION -eq 11 ]]
 						then
 							echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Instalando JAVA na versão 11..."
 							echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Confirme a instalação quando solicitado"
@@ -50,10 +51,12 @@ if [ \"$confirm\" == \"S\" ]
 					echo "Finalizando instalação..."
 					sleep 2
 					clear
+					echo "REDWAY Corp."
 					exit
 			fi
 		fi
 
+	clear
 	echo "$(tput setaf 10)[manoBOT]:$(tput setaf 7) Baixando o sistema man.OS..."
 	sleep 2
 	wget --no-check-certificate --content-disposition -O $HOME/manOS.jar $MANOS_URL
@@ -64,6 +67,7 @@ fi
 echo "Finalizando instalação..."
 sleep 2
 clear
+echo "REDWAY Corp."
 exit
 # ===================================================================
 # All rights reserved: REDWAY Corp.
